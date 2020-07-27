97880e69eb9028e15a4b5883bab72fcae6b440fa
mitchcain <66141416+mitchcain@users.noreply.github.com>
2020-07-27 16:01:51 -0400

    add py file

---
+++ exploratory.py
@@ -0,0 +1,113 @@
+
+# playoff results by year
+# 5 = SC, 4 = SCF, 3 = CF, 2 = R2, 1 = R1, 0 = no playoffs
+result19_ana = [1, 4, 1, 1, 2, 3, 1, 1, 1, 5, 1, 3, 2, 0, 1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+result18_ana = [2, 3, 3, 2, 4, 1, 5, 1, 1, 2, 2, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+result17_ana = [2, 5, 1, 1, 1, 3, 2, 1, 2, 2, 1, 3, 1, 1, 0, 4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+result16_ana = [2, 2, 3, 5, 1, 1, 1, 1, 1, 2, 4, 3, 1, 2, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+result15_ana = [3, 2, 3, 1, 4, 1, 5, 1, 2, 1, 1, 2, 1, 1, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+result14_ana = [2, 2, 1, 1, 1, 2, 3, 1, 5, 3, 2, 4, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+result13_ana = [5, 3, 1, 1, 4, 1, 1, 3, 1, 1, 2, 2, 2, 2, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+result12_ana = [1, 2, 3, 1, 2, 2, 4, 1, 1, 1, 3, 1, 5, 1, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+result11_ana = [4, 2, 2, 1, 3, 2, 5, 3, 1, 2, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+result10_ana = [1, 3, 5, 1, 1, 2, 2, 1, 2, 1, 1, 1, 1, 2, 0, 0, 0, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+result09_ana = [1, 2, 4, 2, 1, 3, 2, 1, 5, 1, 3, 1, 1, 0, 1, 1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+result08_ana = [5, 2, 2, 1, 4, 1, 1, 2, 3, 2, 3, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+result07_ana = [3, 3, 5, 1, 2, 2, 1, 1, 4, 2, 1, 1, 1, 0, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+result06_ana = [1, 2, 5, 1, 3, 1, 1, 2, 1, 1, 2, 3, 4, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+result04_ana = [2, 5, 3, 1, 2, 1, 3, 1, 1, 2, 1, 4, 2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+result03_ana = [3, 2, 1, 5, 2, 1, 2, 1, 1, 4, 3, 2, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+result02_ana = [5, 1, 3, 2, 3, 2, 1, 1, 1, 1, 1, 1, 2, 1, 0, 4, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+result01_ana = [5, 4, 1, 1, 2, 3, 1, 2, 1, 3, 1, 1, 2, 2, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+# playoff wins by year
+plwins19 = [00, 15, 01, 03, 04, 10, 03, 02, 00, 16, 02, 08, 06, 00, 03, 07, 07, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00]
+plwins18 = [07, 09, 11, 05, 13, 03, 16, 00, 01, 06, 06, 00, 02, 01, 02, 00, 02, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00]
+plwins17 = [07, 16, 00, 01, 01, 10, 07, 02, 06, 06, 02, 11, 02, 02, 00, 14, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00]
+plwins16 = [06, 07, 10, 16, 03, 03, 02, 01, 01, 05, 14, 11, 02, 07, 00, 01, 02, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00]
+
+
+import pandas as pd 
+
+# read year by year standings into individual dfs
+df01 = pd.read_csv('/home/mitch/Documents/sc_winners/standings_01.csv')
+df02 = pd.read_csv('/home/mitch/Documents/sc_winners/standings_02.csv')
+df03 = pd.read_csv('/home/mitch/Documents/sc_winners/standings_03.csv')
+df04 = pd.read_csv('/home/mitch/Documents/sc_winners/standings_04.csv')
+df06 = pd.read_csv('/home/mitch/Documents/sc_winners/standings_06.csv')
+df07 = pd.read_csv('/home/mitch/Documents/sc_winners/standings_07.csv')
+df08 = pd.read_csv('/home/mitch/Documents/sc_winners/standings_08.csv')
+df09 = pd.read_csv('/home/mitch/Documents/sc_winners/standings_09.csv')
+df10 = pd.read_csv('/home/mitch/Documents/sc_winners/standings_10.csv')
+df11 = pd.read_csv('/home/mitch/Documents/sc_winners/standings_11.csv')
+df12 = pd.read_csv('/home/mitch/Documents/sc_winners/standings_12.csv')
+df13 = pd.read_csv('/home/mitch/Documents/sc_winners/standings_13.csv')
+df14 = pd.read_csv('/home/mitch/Documents/sc_winners/standings_14.csv')
+df15 = pd.read_csv('/home/mitch/Documents/sc_winners/standings_15.csv')
+df16 = pd.read_csv('/home/mitch/Documents/sc_winners/standings_16.csv')
+df17 = pd.read_csv('/home/mitch/Documents/sc_winners/standings_17.csv')
+df18 = pd.read_csv('/home/mitch/Documents/sc_winners/standings_18.csv')
+df19 = pd.read_csv('/home/mitch/Documents/sc_winners/standings_19.csv')
+
+# add results column
+df01['result'] = result01_ana
+df02['result'] = result02_ana
+df03['result'] = result03_ana
+df04['result'] = result04_ana
+df06['result'] = result06_ana
+df07['result'] = result07_ana
+df08['result'] = result08_ana
+df09['result'] = result09_ana
+df10['result'] = result10_ana
+df11['result'] = result11_ana
+df12['result'] = result12_ana
+df13['result'] = result13_ana
+df14['result'] = result14_ana
+df15['result'] = result15_ana
+df16['result'] = result16_ana
+df17['result'] = result17_ana
+df18['result'] = result18_ana
+df19['result'] = result19_ana
+
+dfs = [df01, df02, df03, df04, df06, df07, df08, df09, df10, df11, df12, df13, df14, df15, df16, df17, df18, df19]
+
+# create a df with only the SC winners
+winners = []
+for df in dfs:
+    dex = df.loc[df['result']==5]
+    winners.append(dex)
+
+winner_df = winners[0]
+num = 1
+while num < len(winners):
+    winner_df = winner_df.append(winners[num])
+    num += 1
+
+years = [2001, 2002, 2003, 2004, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
+winner_df['year'] = years
+winner_df = winner_df.set_index('year')
+
+# create a df with the last place team each year
+losers = []
+for df in dfs:
+    dex = df.iloc[-2:-1]
+    losers.append(dex)
+
+loser_df = losers[0]
+loser_num = 1
+while loser_num < len(losers):
+    loser_df = loser_df.append(losers[loser_num])
+    loser_num += 1
+
+loser_df['year'] = years
+loser_df = loser_df.set_index('year')
+
+# create a df for all teams 2000-01 - 2018-19
+yr = 2001
+for df in dfs:
+    df['year'] = yr
+    yr += 1
+all_data = df01
+for df in dfs[1:]:
+    all_data = all_data.append(df)
+
